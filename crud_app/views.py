2f7f141567289d3fcd622df7041bb7b818215ab3
bachuakshay123 <BACHU.AKSHAY123@GMAIL.COM>
2026-05-25 16:20:51 +0530

    done pagination

--- crud_app/views.py
+++ crud_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from crud_app.models import student
 from crud_app.forms import studentForms
 from django.core.paginator import Paginator
+from django.db.models import Q
+
 
 def home(request):
     form=studentForms()
@@ -15,11 +17,21 @@ def home(request):
     return render(request, "Enter_details.html",{'form':form})
 
 def view_data(request):
-    data=student.objects.all()
-    paginator = Paginator(data, 5)
+    search = request.GET.get('search', '')
+    data = student.objects.all()
+    if search:
+        data = data.filter(
+            Q(first_name__icontains=search) |
+            Q(last_name__icontains=search) |
+            Q(email__icontains=search)
+        )
+    paginator = Paginator(data, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request,'view.html',{'page_obj': page_obj})
+    return render(request, 'view.html', {
+        'page_obj': page_obj,
+        'search': search
+    })
 
 def edit_data(request, id):
     data = student.objects.get(id=id)
